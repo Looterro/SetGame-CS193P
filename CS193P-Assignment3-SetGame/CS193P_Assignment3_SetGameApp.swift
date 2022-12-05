c1e55ac55042edb2e3f24d298c86accdd7a5c1f9
Jakub Łata <lootjakub@gmail.com>
2022-12-05 15:15:51 +0100

    Initial Commit

---
+++ CS193P-Assignment3-SetGame/CS193P_Assignment3_SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193P_Assignment3_SetGameApp.swift
+//  CS193P-Assignment3-SetGame
+//
+//  Created by Jakub Łata on 05/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CS193P_Assignment3_SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS193P-Assignment3-SetGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CS193P-Assignment3-SetGame
+//
+//  Created by Jakub Łata on 05/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
